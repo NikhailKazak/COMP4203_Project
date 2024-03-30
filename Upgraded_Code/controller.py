68dc12940579d96744285ed8c4bd4366617280b8
Nikhail Kazak <NKazak1@outlook.com>
2024-03-30 14:00:46 -0400

    Made some changes

--- Upgraded_Code/controller.py
+++ Upgraded_Code/controller.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     while promptInput!="4":
 
         print("Please begin by choosing a mode (the numeric value) from those listed below:")
-        print("1. Basic BLE Scan\n2. Device based tracking/metrics \n3. ETC (TBD) \n4. Exit Program\n")
+        print("1. Basic BLE Scan\n2. Scan for BLE devices and get metrics \n3. Get Metrics - assumes a BLE Scan has already been done \n4. Exit Program\n")
 
 
         #Take input
@@ -47,7 +47,8 @@ if __name__ == "__main__":
 
             opt1=opt1[0:17]
 
-            print("Retrieving entries for ", opt1)
+            print("Generating and retrieving entries for ", opt1)
+            print("For more info on {} refer to ../filteredBAddr.txt".format(opt1))
             reorganizedEntries=funcBAddr.reorgByBAddr(opt1)
 
             avgSize=runStats.getAvgSizePacket()
@@ -60,6 +61,34 @@ if __name__ == "__main__":
 
         elif(promptInput=="3"):
             print("You've chosen option 3")
+
+            #Generates a return of all unique BAddr addresses
+            unique_addresses=funcBAddr.getUniqueBAddrs()
+
+            print("\nHere's a list of unique Bluetooth addresses found from the BLE scan\n")
+            for addr in unique_addresses:
+                print(addr)
+
+            print("Which address do you wish to gather metrics for? Please enter the full address and device ( e.g., 00:04:4B:F9:01:DB :: NVIDIA SHIELD Remote )\n")
+
+            opt1=""
+
+            while(opt1 not in unique_addresses):
+                opt1=str(input())
+
+            opt1=opt1[0:17]
+
+            print("Generating and retrieving entries for ", opt1)
+            print("For more info on {} refer to ../filteredBAddr.txt".format(opt1))
+            reorganizedEntries=funcBAddr.reorgByBAddr(opt1)
+
+            avgSize=runStats.getAvgSizePacket()
+            print("Average packet size in bytes: ", avgSize)
+
+            avgTime=runStats.getAvgTimeBetweenPackets()
+            print("Average time between packets in seconds: ", avgTime)
+
+            print("\n")
 
     print("Goodbye")
     exit
