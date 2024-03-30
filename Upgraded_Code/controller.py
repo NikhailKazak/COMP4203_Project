15b0c172b10b61f95e4a4c2a72ff71a7f51b1ac6
Nikhail Kazak <NKazak1@outlook.com>
2024-03-30 15:28:03 -0400

    Made some changes

--- Upgraded_Code/controller.py
+++ Upgraded_Code/controller.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     
     promptInput="0"
 
-    while promptInput!="4":
+    while promptInput!="5":
 
         print("Please begin by choosing a mode (the numeric value) from those listed below:")
-        print("1. Basic BLE Scan\n2. Scan for BLE devices and get metrics \n3. Get Metrics - assumes a BLE Scan has already been done \n4. Exit Program\n")
+        print("1. Basic BLE Scan\n2. Scan for BLE devices and get metrics \n3. Get Metrics - assumes a BLE Scan has already been done \n4. Output device specific info (note: requires pre-existing device metrics) \n5. Exit Program\n")
 
 
         #Take input
@@ -38,7 +38,7 @@ if __name__ == "__main__":
             for addr in unique_addresses:
                 print(addr)
 
-            print("Which address do you wish to gather metrics for? Please enter the full address and device ( e.g., 00:04:4B:F9:01:DB :: NVIDIA SHIELD Remote )\n")
+            print("\nWhich address do you wish to gather metrics for? Please enter the full address and device ( e.g., 00:04:4B:F9:01:DB :: NVIDIA SHIELD Remote )\n")
 
             opt1=""
 
@@ -89,6 +89,15 @@ if __name__ == "__main__":
             print("Average time between packets in seconds: ", avgTime)
 
             print("\n")
+        
+        elif(promptInput=="4"):
+            print("You've chosen option 4")
 
-    print("Goodbye")
+            with open("filteredBAddr.txt", "r") as file:
+                for line in file:
+                    print(line, end="")
+
+            print("~~~\n")
+
+    print("Goodbye\n")
     exit
